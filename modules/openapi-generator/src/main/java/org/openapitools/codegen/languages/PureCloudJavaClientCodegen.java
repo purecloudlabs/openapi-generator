7585720e6b5132564eba92864cd789f71d636633
Michael Roddy <88041719+michaeljroddy@users.noreply.github.com>
2022-09-28 15:47:15 +0100

    java sdk bug fix MatchCriteria

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/PureCloudJavaClientCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/PureCloudJavaClientCodegen.java
@@ -286,7 +286,11 @@ public class PureCloudJavaClientCodegen extends JavaClientCodegen {
         if (ModelUtils.isMapSchema(schema)) {
             // API-2916 default values for Map properties cause unexpected issues for PUT requests
             return "null";
-        } else {
+        } else if (ModelUtils.isArraySchema(schema)) {
+            // BUG FIX Lists default value should be null
+            return "null";
+        }
+        else {
             return super.toDefaultValue(schema);
         }
     }
