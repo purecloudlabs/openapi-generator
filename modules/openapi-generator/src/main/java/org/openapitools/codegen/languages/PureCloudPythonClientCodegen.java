f9b4c46e8b0cd7d408c6f35eb13cc8cf991bf332
Michael Roddy <michael.roddy@genesys.com>
2023-04-03 12:14:32 +0100

    type hinting support python

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/PureCloudPythonClientCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/PureCloudPythonClientCodegen.java
@@ -72,6 +72,54 @@ public class PureCloudPythonClientCodegen extends PythonClientCodegen {
             Map<String, Object> mo = (Map<String, Object>) _mo;
             CodegenModel cm = (CodegenModel) mo.get("model");
             cm.classFilename = cm.classFilename.replaceAll("\\_{1,}$", "_");
+            // Determine the python "type hinting" data type for model properties
+            List<CodegenProperty> vars = cm.vars;
+            if (vars != null) {
+                for (CodegenProperty cp : vars) {
+                    if (cp.dataType.contains("list[")) {
+                        String item = cp.dataType.substring(cp.dataType.indexOf('[')+1, cp.dataType.indexOf(']'));
+                        if (Character.isUpperCase(item.charAt(0))) {
+                            String newDataType = "List" + "[" + "'" + item + "'" + "]";
+                            cp.vendorExtensions.put("x-dataType", newDataType);
+                        } else if (item.contains("dict(")) {
+                            String key =  item.substring(item.indexOf('(') + 1, item.indexOf(','));
+                            String value =  item.substring(item.indexOf(' ') + 1, item.indexOf(')'));
+                            String newDataType = "List" + "[" + "Dict" + "[" + key + "," + " " + value + "]" + "]";
+                            System.out.println(newDataType);
+                            cp.vendorExtensions.put("x-dataType", newDataType);
+                        } else {
+                            String newDataType = "List" + "[" + item + "]";
+                            cp.vendorExtensions.put("x-dataType", newDataType);
+                        }
+                    } else if (cp.dataType.contains("dict(")) {
+                        String key =  cp.dataType.substring(cp.dataType.indexOf('(') + 1, cp.dataType.indexOf(','));
+                        String value =  cp.dataType.substring(cp.dataType.indexOf(' ') + 1, cp.dataType.indexOf(')'));
+                        if (value.contains("list[")) {
+                            String item = cp.dataType.substring(cp.dataType.indexOf('[') + 1, cp.dataType.indexOf(']'));
+                            if (Character.isUpperCase(item.charAt(0))) {
+                                String newDataType = "Dict" + "[" + key + "," + " " + "List" + "[" + "'" + item + "'" + "]";
+                                cp.vendorExtensions.put("x-dataType", newDataType);
+                            } else {
+                                String newDataType = "Dict" + "[" + key + "," + " " + "List" + "[" + item + "]";
+                                cp.vendorExtensions.put("x-dataType", newDataType);
+                            }
+                        }
+                        if (Character.isUpperCase(value.charAt(0))) { // value is a model
+                            String newValue = "'" + value + "'";
+                            String newDataType = "Dict" + "[" + key + "," + " " + newValue + "]";
+                            cp.vendorExtensions.put("x-dataType", newDataType);
+                        } else {
+                            String newDataType = "Dict" + "[" + key + "," + " " + value + "]";
+                            cp.vendorExtensions.put("x-dataType", newDataType);
+                        }
+                    } else if (cp.isModel || Character.isUpperCase(cp.dataType.charAt(0))) {
+                        String newDataType = "'" + cp.dataType + "'";
+                        cp.vendorExtensions.put("x-dataType", newDataType);
+                    } else {
+                        cp.vendorExtensions.put("x-dataType", cp.dataType);
+                    }
+                }
+            }
         }
         return objs;
     }
@@ -101,15 +149,40 @@ public class PureCloudPythonClientCodegen extends PythonClientCodegen {
             if (operations != null) {
                 List<CodegenOperation> ops = (List<CodegenOperation>) operations.get("operation");
                 for (CodegenOperation operation : ops) {
-                    if (operation.returnType != null) {
-                        String jsonSchema = operation.responses.get(0).jsonSchema;
-                        if (isEmpty(jsonSchema)) {
-                            operation.returnType = "Empty";
-                        }
-                        if (jsonSchema.contains("additionalProperties") && jsonSchema.contains("object") && jsonSchema.contains("type")) {
-                            operation.returnType = "dict[str, Any]";
+                    List<CodegenParameter> allParams = operation.allParams;
+                    // Determine the python "type hinting" data type for required operation parameters
+                    for (CodegenParameter p : allParams) {
+                        if (p.required) {
+                            if (p.dataType.contains("list[")) {
+                                String item = p.dataType.substring(p.dataType.indexOf('[')+1, p.dataType.indexOf(']'));
+                                String newDataType = "List[" + "'" + item + "']";
+                                p.vendorExtensions.put("x-dataType", newDataType);
+                            } else if (p.isModel || Character.isUpperCase(p.dataType.charAt(0))) {
+                                String newDataType = "'" + p.dataType + "'";
+                                p.vendorExtensions.put("x-dataType", newDataType);
+                            } else {
+                                p.vendorExtensions.put("x-dataType", p.dataType);
+                            }
                         }
                     }
+                    // Determine the python "type hinting" data type for operation return types
+                    if (operation.returnType != null) {
+                        if (operation.returnType.contains("dict(")) {
+                            String key =  operation.returnType.substring(operation.returnType.indexOf('(') + 1, operation.returnType.indexOf(','));
+                            String value =  operation.returnType.substring(operation.returnType.indexOf(' ') + 1, operation.returnType.indexOf(')'));
+                            String newDataType = "Dict" + "[" + key + "," + " " + value + "]";
+                            operation.vendorExtensions.put("x-returnType", newDataType);
+                        } else if (operation.returnType.contains("list[")) {
+                            String newDataType = "List[" + "'" + operation.returnType.substring(operation.returnType.indexOf('[')+1, operation.returnType.indexOf(']')) + "']";
+                            operation.vendorExtensions.put("x-returnType", newDataType);
+                        } else if (!operation.returnTypeIsPrimitive && !operation.returnType.contains("list[")) {
+                            String newDataType = "'" + operation.returnType + "'";
+                            operation.vendorExtensions.put("x-returnType", newDataType);
+                        } else {
+                            operation.vendorExtensions.put("x-returnType", operation.returnType);
+                        }
+                    }
+
                 }
             }
         }
